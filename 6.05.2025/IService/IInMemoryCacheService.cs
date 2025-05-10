a311ebd99bcf627a309be5425256977443667619
mariamrevazashvili <mariamrevazashvili25@gmail.com>
2025-05-10 14:26:49 +0400

    Add project files.

---
+++ 6.05.2025/IService/IInMemoryCacheService.cs
@@ -0,0 +1,8 @@
+﻿namespace _6._05._2025.IService
+{
+    public interface IInMemoryCacheService
+    {
+        T Get<T>(string key);
+        void Set<T>(string key, T value, TimeSpan expirationDuration);
+    }
+}
---
+++ 6.05.2025/Service/InMemoryCacheService.cs
@@ -0,0 +1,60 @@
+﻿using _6._05._2025.IService;
+
+namespace _6._05._2025.Service
+{
+    // 6.05.2025 && 10.05.2025
+    public class InMemoryCacheService : IInMemoryCacheService
+    {
+        private static InMemoryCacheService _unique;
+        private class CacheItem
+        {
+            public object Value { get; set; }
+            public DateTime Expiration { get; set; }
+        }
+
+        private Dictionary<string, CacheItem> _cache = new();
+
+        private InMemoryCacheService()
+        {
+            _cache = new Dictionary<string, CacheItem>();
+        }
+
+        public static InMemoryCacheService GetInstance()
+        {
+            if (_unique == null)
+            {
+                _unique = new InMemoryCacheService();
+            }
+
+            return _unique;
+        }
+
+        public T Get<T>(string key)
+        {
+            if (_cache.TryGetValue(key, out var item))
+            {
+                if (DateTime.UtcNow <= item.Expiration)
+                {
+                    return (T)item.Value;
+                }
+                else
+                {
+                    _cache.Remove(key);
+                }
+            }
+
+            return default;
+        }
+
+        public void Set<T>(string key, T value, TimeSpan expirationDuration)
+        {
+            var item = new CacheItem
+            {
+                Value = value,
+                Expiration = DateTime.UtcNow.Add(expirationDuration)
+            };
+
+            _cache[key] = item;
+        }
+    }
+}
